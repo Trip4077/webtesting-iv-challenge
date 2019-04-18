c09b7d71dbe471afb69744f8b954ed3c505c4368
Trip4077 <bj2336@email.vccs.edu>
2019-04-18 09:01:04 -0400

    inventory model created

---
+++ inventory/model.js
@@ -0,0 +1,19 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+    insert,
+    remove,
+    getAll
+}
+
+async function insert(vehicle) {
+    return null;       
+}
+
+async function remove(id) {
+    return null;
+}
+
+async function getAll() {
+    return null;
+}
