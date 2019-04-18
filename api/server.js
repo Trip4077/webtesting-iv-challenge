21b1d4f6df85ba89d43b508daefa31c45584f928
Trip4077 <bj2336@email.vccs.edu>
2019-04-18 08:35:29 -0400

    Server Configured

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const server = express();
+
+// const Inventory = require('../inventory/model');
+
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+    res.status(200).json({ api: 'runnning' });
+});
+
+// server.get('/inventory', async (req, res) => {
+//     const stock =  await Inventory.getAll();
+
+//     res.status(200).json(stock);
+// });
+
+module.exports = server; 
