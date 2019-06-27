9d69d53ee2cfa4359a981a3a7bd04996d56588c1
Trip4077 <bj2336@email.vccs.edu>
2019-06-27 11:08:07 -0400

    Server Test Suite Created

---
+++ server/server.test.js
@@ -0,0 +1,44 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe("SERVER", () => {
+    describe("Environment", () => {
+        it('should set test environment', () => {
+            const env = process.env.DB_CONNECT;
+
+            expect(env).toBe('testing');
+        });
+    });
+
+    describe("GET", () => {
+        describe('/', () => {
+            it('should return a JSON packet with status 200', () => {
+                const res = await request(server).get('/');
+    
+                expect(res.status).toBe(200);
+                expect(res.type).toBe('application/json');
+            });
+    
+            it('should return the message \'server runnning\'', () => {
+                const res = await request(server).get('/');
+    
+                expect(res.body).toBe({ api: "Server Running" });
+            });
+        });
+
+        describe('/inventory', () => {
+            it('should return a JSON packet with status 200', () => {
+                const res = await request(server).get('/invetory');
+
+                expect(res.status).toBe(200);
+                expect(res.type).toBe('application/json');
+            });
+
+            it('should return an array', () => {
+                const res = await request(server).get('/inventory');
+
+                expect(Array.isArray(res.body)).toBe(true);
+            });
+        });
+    });
+});
